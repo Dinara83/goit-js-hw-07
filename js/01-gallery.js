20ac285d72e2419630e742be0a4cefb8b055bb37
Dinara83 <108099459+Dinara83@users.noreply.github.com>
2022-11-10 22:15:36 +0200

    розмітка gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  galleryEl: document.querySelector('.gallery'),
+};
+
+const makeGalleryItemsColection = ({ preview, original, description }) => {
+  return `<div class="gallery__item">
+<a class="gallery__link" href="${original}">
+  <img
+	class="gallery__image"
+	src="${preview}"
+	data-source="${original}"
+	alt="${description}"
+  />
+</a>
+</div>`;
+};
+
+const makeColectionImage = galleryItems.map(makeGalleryItemsColection).join('');
+refs.galleryEl.insertAdjacentHTML('beforeend', makeColectionImage);
+const imagEl = document.querySelector('.gallery__image');
+console.log(imagEl);
+
+imagEl.addEventListener('click', onImageInColection);
+
+function onImageInColection(evt) {
+  console.log();
+}
