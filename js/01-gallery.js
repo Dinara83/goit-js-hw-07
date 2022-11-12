147419e48a2a3cc2b4485b1cdb11962c008b3dad
Dinara83 <108099459+Dinara83@users.noreply.github.com>
2022-11-12 12:13:55 +0200

    show image

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,14 +2,16 @@ import { galleryItems } from './gallery-items.js';
 
 const refs = {
   galleryEl: document.querySelector('.gallery'),
+  body: document.querySelector('body'),
+  //   imagEl: document.querySelector('gallery__link'),
 };
 
 const makeGalleryItemsColection = ({ preview, original, description }) => {
   return `<div class="gallery__item">
 <a class="gallery__link" href="${original}">
-  <img
-	class="gallery__image"
-	src="${preview}"
+  <img 
+	class="gallery__image lazyload"
+	data-src="${preview}"
 	data-source="${original}"
 	alt="${description}"
   />
@@ -18,12 +20,26 @@ const makeGalleryItemsColection = ({ preview, original, description }) => {
 };
 
 const makeColectionImage = galleryItems.map(makeGalleryItemsColection).join('');
+
 refs.galleryEl.insertAdjacentHTML('beforeend', makeColectionImage);
-const imagEl = document.querySelector('.gallery__image');
-console.log(imagEl);
+refs.body.addEventListener('click', onOpenColectionClick);
 
-imagEl.addEventListener('click', onImageInColection);
+function onOpenColectionClick(evt) {
+  evt.preventDefault();
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+  window.instance = basicLightbox.create(`
+<img src="${evt.target.dataset.source}">
+`);
+  instance.show();
+}
 
-function onImageInColection(evt) {
-  console.log();
+const lazyImages = document.querySelectorAll('.lazyload');
+lazyImages.forEach(image => {
+  image.addEventListener('load', onImageLoaded, { once: true });
+});
+
+function onImageLoaded(evt) {
+  evt.target.classList.add('appear');
 }
