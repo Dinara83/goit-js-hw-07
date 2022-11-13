79e7e22bb00048975d0690f8618fe8d309e5751e
Dinara83 <108099459+Dinara83@users.noreply.github.com>
2022-11-13 15:42:53 +0200

    слайд - 2 задание

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,50 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-import { addGalleryItems } from './gallery-items.js';
 console.log(galleryItems);
+const refs = {
+  galleryEl: document.querySelector('.gallery'),
+  body: document.querySelector('body'),
+};
+
+const makeGalleryItemsColection = ({ preview, original, description }) => {
+  return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image lazyload" src="${preview}" alt="${description}" />
+</a>`;
+};
+const makeColectionImage = galleryItems.map(makeGalleryItemsColection).join('');
+
+refs.galleryEl.insertAdjacentHTML('beforeend', makeColectionImage);
+refs.body.addEventListener('click', onOpenGalleryColection);
+
+function onOpenGalleryColection(evt) {
+  evt.preventDefault();
+  const isGalleryImage = evt.target.classList.contains('gallery__image');
+  if (!isGalleryImage) {
+    return;
+  }
+  window.instance = basicLightbox.create(
+    `
+  	  <img src="${evt.target.dataset.source}">
+  	  `,
+  );
+  instance.show();
+  refs.galleryEl.addEventListener('keydown', evt => {
+    if (evt.key === 'Escape') {
+      instance.close();
+    }
+  });
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  scrollZoom: false,
+});
+
+const lazyImages = document.querySelectorAll('.lazyload');
+lazyImages.forEach(image => {
+  image.addEventListener('load', onImageLoaded, { once: true });
+});
+
+function onImageLoaded(evt) {
+  evt.target.classList.add('appear');
+}
